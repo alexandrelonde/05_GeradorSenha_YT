f42541dd8e4f59872debaf8a32ee544ead0a9612
Alexandre Londe <alexandrelonde.x@gmail.com>
2023-01-15 23:44:33 -0300

    Inserindo a função de gerar a senha randomicamente

--- script.js
+++ script.js
@@ -8,11 +8,26 @@ let password = document.querySelector("#password");
 let containerPassword = document.querySelector("#container-password");
 
 // Iniciando a lógica de programação
-let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@"
 let novaSenha ="";
 
-
+// Setando o valor do slide como texto
 sizePassword.innerHTML = sliderElement.value;
 slider.oninput = function(){
     sizePassword.innerHTML = this.value;
 }
+
+// Função para pegar a senha randomicamente com o tamanho sizePassword
+function generatePassword(){
+    let pass = "";
+    for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
+        pass += charset.charAt(Math.floor(Math.random() * n));
+    }
+
+    containerPassword.classList.remove("hide");
+    password.innerHTML = pass;
+}
+
+// Math.floor -> para gerar um número inteiro
+// Math. random -> para gerar um número aleatório
+// n -> tamanho do charset
