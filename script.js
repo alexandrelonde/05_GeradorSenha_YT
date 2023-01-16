5c3757886c67302be1abfa9260c9231c2a7386b7
Alexandre Londe <alexandrelonde.x@gmail.com>
2023-01-15 22:52:48 -0300

    Coletando os ids dos elementos no arquivo script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+//Coletando os ids dos elementos
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button");
+
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("#password");
+
+let containerPassword = document.querySelector("#container-password");
